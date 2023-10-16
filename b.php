c24d0a2429c89f761b94aefcec0bb1e8edfdb608
LAPTOP-D6HHCEHC\SHENBAO <baiyesu664@gmail.com>
2023-10-16 11:35:07 +0800

    version1.2

--- b.php
+++ b.php
@@ -3,5 +3,5 @@ Page 2, Page 2, this is page 2
 <?php
 $ID = $_POST["userID"];
 $Name = $_POST["userName"];
-echo "I got your form data: userID=$ID, userName=$Name";
+echo ": userID=$ID, userName=$Name";
 ?>
